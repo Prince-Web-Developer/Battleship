c7b7a5236e451f9b4b2a3cbaffc98117a3d7d5bc
Prince-Web-Developer <268293963+Prince-Web-Developer@users.noreply.github.com>
2026-07-02 07:16:31 +0500

    feat(ship): implement ship class

---
+++ src/scripts/class.js
@@ -0,0 +1,23 @@
+class Ship{
+    #hits
+    #length
+    constructor(name, length) {
+        if (length <= 0) throw new Error("Length should be more than 0")
+        this.name = name
+        this.#length = length
+        this.#hits = 0
+    }
+
+
+    hit() {
+        this.#hits += 1
+    }
+
+    isSunk() {
+        return this.#hits >= this.#length
+    }
+
+}
+
+
+
