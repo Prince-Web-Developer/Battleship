3c423e1a87fed8f794873d219a387485b12ce55f
Prince-Web-Developer <268293963+Prince-Web-Developer@users.noreply.github.com>
2026-07-12 19:16:45 +0500

    feat: complete play screen ui

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import css from "./style.css";
 import mainCanvas from "./scripts/ui/mainCanvas.js"
 import navigation from "./scripts/ui/navigation.js"
+
+
+
+document.querySelector("#gameMode").addEventListener("submit", (e) => {
+    e.preventDefault()
+})
